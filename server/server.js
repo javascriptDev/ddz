d62c9071fbd854a87d1c68dd337096b27e5c5945
gaoyuqiao <gaoyuqiao@hotmail.com>
2014-06-20 16:18:01 +0800

    base file

--- server/server.js
+++ server/server.js
@@ -3,40 +3,72 @@
  */
 var http = require('http');
 var fs = require('fs');
+var table = require('Table');
+var Player = require('Player');
+var Poker = require('Poker');
+
+
+//所有斗地主的房间
+var rooms = [
+    {id: 1111, players: []}
+];
+
+//所有登陆的用户
+var players = [];
+
+function login(res) {
+    if (rooms[0].players.length == 3) {
+        var pokers = getPoker();
+
+    } else {
+        var player = JSON.parse(postDataChunk.toString());
+        rooms[0].players.push();
+        var uid = Math.random() * 19999,
+            roomId = rooms[0].id;
+        // 设置cookie
+        res.setHeader("Set-Cookie", ['uid=' + Math.random() * 19999, 'roomid=' + roomId]);
+        players.push({
+            success: true,
+            id: uid,
+            roomId: roomId
+        })
+        res.write(JSON.stringify({
+            success: true
+        }));
+        res.end();
+    }
+}
+function getPoker(req, res) {
+
+    return new table().poker;
+
+}
 
 http.createServer(function (req, res) {
-
-    var rooms = [];
-    var players = [];
-
-    req.addListener("data", function (postDataChunk) {
-        if (url.indexOf('server') != -1) {
-
-        }
-    });
-    // 数据接收完毕，执行回调函数
-    req.addListener("end", function () {
-        if (url.indexOf('AA') != -1) {
-
-
-        }
-    });
-
     var url = req.url;
+    req.on("data", function (postDataChunk) {
+        if (url.indexOf('login') != -1) {
+            login(res);
+        } else if (url.indexOf('getPoker') != -1) {
+            getPoker(req, res);
+        }
+    });
+
 
     if (url == '/') {
         res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-        fs.readFile('index.html', 'utf8', function (err, data) {
+        fs.readFile('client/index.html', 'utf8', function (err, data) {
             res.write(data);
             res.end();
         });
 
-    } else if (url.indexOf('server') != -1) {
-//
+    } else if (url.indexOf('login') != -1) {
+
     } else {
         var a = url.split('.')[1] == 'js' ? 'application/x-javascript' : 'text/css';
         var u = url.replace('/', '');
-        res.writeHead(200, {'Content-Type': +a + ';charset=utf-8'});
+        var head = a + ';charset=utf-8';
+        res.writeHead(200, {'Content-Type': head});
         if (u != 'favicon.ico') {
             fs.readFile(url.replace('/', ''), 'utf8', function (err, data) {
                 res.write(data);
